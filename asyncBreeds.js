cf6a08c282339aa25d7e6383280e4ef7bb0dd53b
Katherine Struthers <ktstruthers@gmail.com>
2023-05-14 13:40:26 -0500

    add both sync and async tests

---
+++ asyncBreeds.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, done) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    error ? done(undefined) : done(data);
+  });
+};
+
+module.exports = breedDetailsFromFile;
